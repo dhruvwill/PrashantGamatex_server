70b6143bbe8ca1e7ea1897c21337a1176ae9d8e0
Dhruvwill <ddyhs25@gmail.com>
2024-06-14 15:29:12 +0530

    Test Database connected, Prisma schema updated

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,38 @@
 import express, { Express, Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
+import cors from "cors";
 
 const app: Express = express();
 const prisma = new PrismaClient();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TS Server");
+});
+
+app.post("/login", async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  console.log(username, password);
+  try {
+    const user = await prisma.user.findUnique({
+      where: { username: username, password: password },
+    });
+
+    if (!user) {
+      return res.status(401).json({ error: "Invalid email or password" });
+    }
+    res.json({
+      token: "test_token",
+      username: user.username,
+      usercode: user.uid,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Login failed" });
+  }
 });
 
 app.listen(port, () => {
